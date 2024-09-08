a1cee53b88518e4ad7dd05c860ac8b631d2779e3
edu <eduardogm81@gmail.com>
2024-09-08 16:04:59 +0200

    Añadidos servicios, Outputs e Imputs

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from "../../interfaces/character.interface";
 
 @Component({
   selector: 'dbz-list',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  @Output()
+  onDelete: EventEmitter<number> = new EventEmitter();
+
+  @Input()
+  public charcterList: Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }];
+
+  // onDelete index value : number emitir
+
+  onDeleteCharacter(index: number): void {
+    // TODO: Emitir el ID del personaje
+    console.log({index});
+    this.onDelete.emit(index);
+  }
 
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import { Character } from "../interfaces/character.interface";
+import { DbzService } from "../services/dbz.service";
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -6,9 +8,12 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class MainPageComponent implements OnInit {
-  constructor() {
+
+  constructor(public dbzService: DbzService) {
   }
 
   ngOnInit() {
   }
+
+
 }
